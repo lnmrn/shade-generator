629c7cb6b9fc51c2d28275bc2cdf20b35441834e
lnmrn <lnmrn.dev@gmail.com>
2026-07-05 14:42:51 +0200

    Add input styles and a color picker

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,11 +11,19 @@ function Form() {
       <h4>Shade generator</h4>
       <form className="color-form">
         <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
           type="text"
           value={color}
           onChange={(e) => setColor(e.target.value)}
           placeholder="#f15025"
         />
+        <button className="btn" type="submit" style={{ background: color }}>
+          Generate
+        </button>
       </form>
     </section>
   );
